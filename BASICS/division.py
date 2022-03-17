72332b6d18a8d838a7dd517a2849a7baf2b2d519
Baibhav Tiwari <baibhavtiwari37@gmail.com>
2022-03-17 21:19:56 +0530

    Add files via upload

---
+++ BASICS/division.py
@@ -0,0 +1,4 @@
+x = int(input("enter the divisor:\n"))
+y = int(input("enter the dividend:\n"))
+z = x % y
+print("the remainder of the dividend is :", z)
---
+++ BASICS/multiplication.py
@@ -0,0 +1,4 @@
+a, b = float(input("enter the number:\n")), float(
+    input("enter the second number:\n"))
+c = a*b
+print("the multiplication of the two number is:", c)
